d39951c5c4d4b2b9fa50e3ee63d7568f3c189cda
Vladimir Mazheru <vmazheru@salzinger.com>
2012-12-26 14:34:19 -0500

    Code formatting

--- town-container-android/src/com/mlsdev/android/townwizard/CategoriesActivity.java
+++ town-container-android/src/com/mlsdev/android/townwizard/CategoriesActivity.java
@@ -28,50 +28,47 @@ public class CategoriesActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.categories);
-	mCategoriesAdapter = new CategoriesAdapter(this);
-	Bundle extras = getIntent().getExtras();
-	final String[] params = { extras.getString(TownWizardConstants.PARTNER_ID), extras.getString(TownWizardConstants.URL) };
-	mPartnerName = extras.getString(TownWizardConstants.PARTNER_NAME);
-	mImageUrl = extras.getString(TownWizardConstants.IMAGE_URL);
-	ImageView iv = (ImageView) findViewById(R.id.iv_categories_header);
-	Log.d("imageUrl", mImageUrl);
-	if (mImageUrl.length() > 0) {
-	    new DownloadImageHelper(iv).execute(DEFAULT_URL + mImageUrl);
-	}
-	GridView gridView = (GridView) findViewById(R.id.gridView);
-	TextView tv = (TextView) findViewById(R.id.tv_categories_header);
-	tv.setText(mPartnerName);
-	gridView.setAdapter(mCategoriesAdapter);
-	gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-	    @Override
-	    public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-		Categories item = (Categories) mCategoriesAdapter.getItem(position);
-		if (item.getUrl().startsWith("http")){
-		    //What is this?
-		}
-		Log.d("url", params[1] + item.getUrl());
-		startBrowser(params[1], item.getUrl(), mPartnerName + " - " + item.getName());
-
-	    }
-	});
-	new SearchCategories(mCategoriesAdapter).execute(params);
+        super.onCreate(savedInstanceState);        
+        setContentView(R.layout.categories);
+        mCategoriesAdapter = new CategoriesAdapter(this);
+        Bundle extras = getIntent().getExtras();
+        final String[] params = {
+                extras.getString(TownWizardConstants.PARTNER_ID),
+                extras.getString(TownWizardConstants.URL) };
+        mPartnerName = extras.getString(TownWizardConstants.PARTNER_NAME);
+        mImageUrl = extras.getString(TownWizardConstants.IMAGE_URL);
+        ImageView iv = (ImageView) findViewById(R.id.iv_categories_header);
+        Log.d("imageUrl", mImageUrl);
+        if (mImageUrl.length() > 0) {
+            new DownloadImageHelper(iv).execute(DEFAULT_URL + mImageUrl);
+        }
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        TextView tv = (TextView) findViewById(R.id.tv_categories_header);
+        tv.setText(mPartnerName);
+        gridView.setAdapter(mCategoriesAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Categories item = (Categories) mCategoriesAdapter.getItem(position);
+                Log.d("url", params[1] + item.getUrl());
+                startBrowser(params[1], item.getUrl(), mPartnerName + " - "
+                        + item.getName());
+            }
+        });
+        new SearchCategories(mCategoriesAdapter).execute(params);
     }
 
     protected void startBrowser(String urlSite, String urlSection, String name) {
-	Intent web = new Intent(this, WebActivity.class);
-	web.putExtra(TownWizardConstants.URL_SITE, urlSite);
-	web.putExtra(TownWizardConstants.URL_SECTION, urlSection);
+        Intent web = new Intent(this, WebActivity.class);
+        web.putExtra(TownWizardConstants.URL_SITE, urlSite);
+        web.putExtra(TownWizardConstants.URL_SECTION, urlSection);
 
-	ImageView iv = (ImageView) findViewById(R.id.iv_categories_header);
-	Drawable drawable = iv.getDrawable();
-	Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
-	web.putExtra(TownWizardConstants.HEADER_IMAGE, bitmap);
-	web.putExtra(TownWizardConstants.PARTNER_NAME, name);
-	startActivity(web);
+        ImageView iv = (ImageView) findViewById(R.id.iv_categories_header);
+        Drawable drawable = iv.getDrawable();
+        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+        web.putExtra(TownWizardConstants.HEADER_IMAGE, bitmap);
+        web.putExtra(TownWizardConstants.PARTNER_NAME, name);
+        startActivity(web);
     }
-
 
 }
