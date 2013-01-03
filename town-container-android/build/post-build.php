6ee706074fe1a9158ee21fa4433e3607b1c61683
Vladimir Mazheru <vmazheru@salzinger.com>
2013-01-03 15:56:03 -0500

    Add task to save partner apk file in deploy directory after the build is done.  Added deploy directory to git ignore

--- town-container-android/build/post-build.php
+++ town-container-android/build/post-build.php
@@ -4,11 +4,14 @@
 
 include 'resources.php';
 
+define("RELEASE_FILE", 'bin/town-container-android-release.apk');
+
 function main() {
 	print "========== Start post-build.php =============\n";
 	$isError = false;
 	try {		
 		restoreResources();
+		saveReleaseFile();
 	} catch (Exception $e) {
 		print $e->getMessage();
 		$isError = true;
@@ -16,6 +19,18 @@ function main() {
 
 	print "========== Done  post-build.php =============\n";
 	if($isError) return 1;
+}
+
+function saveReleaseFile() {
+	$partnerId = getPartnerId();
+	mkdir("deploy");
+	mkdir("deploy/$partnerId");
+	
+	$src = RELEASE_FILE;
+	$dest = "deploy/$partnerId/".end(explode('/', $src));	
+	
+	print "Copying: $src -> $dest\n";
+	copy($src, $dest);
 }
 
 main();
