6fcddabc66d874956ee893a766b66800cbb1a909
Vladimir Mazheru <vmazheru@salzinger.com>
2013-02-06 15:27:43 -0500

    Working FB login.  Just the login, no UI with friends or locations yet

--- town-container-android/src/com/townwizard/android/FacebookPlacesActivity.java
+++ town-container-android/src/com/townwizard/android/FacebookPlacesActivity.java
@@ -1,13 +1,100 @@
 package com.townwizard.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
+
+import com.facebook.LoggingBehavior;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.Settings;
 
 public class FacebookPlacesActivity extends FragmentActivity {
 
+    private Session.StatusCallback statusCallback = new SessionStatusCallback();
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.facebook_places);
-    }    
+        
+        checkLogin(savedInstanceState);
+        showPlaces();
+    }
+    
+    
+    private void checkLogin(Bundle savedInstanceState) {
+        Settings.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
+        
+        Session session = Session.getActiveSession();        
+        if (session == null) {
+            if (savedInstanceState != null) {
+                session = Session.restoreSession(this, null, statusCallback, savedInstanceState);
+            }
+        }        
+        if (session == null) {            
+            session = new Session(this);
+                       
+        }        
+        
+        Session.setActiveSession(session); 
+
+        if (session.getState().equals(SessionState.CREATED_TOKEN_LOADED)) {
+            session.openForRead(new Session.OpenRequest(this).setCallback(statusCallback));
+        }
+
+        if (!session.isOpened() && !session.isClosed()) {
+            session.openForRead(new Session.OpenRequest(this).setCallback(statusCallback));
+        } else {
+            Session.openActiveSession(this, true, statusCallback);
+        }
+    }
+        
+    private void showPlaces() {
+        
+    }
+    
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateView();
+        Session.getActiveSession().addCallback(statusCallback);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Session.getActiveSession().removeCallback(statusCallback);
+    }
+    
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Session session = Session.getActiveSession();
+        Session.saveSession(session, outState);
+    }
+
+    private void updateView() {
+        TextView v = (TextView)findViewById(R.id.login_status);
+        Session session = Session.getActiveSession();
+        if (session != null && session.isOpened()) {
+            v.setText(session.getState().toString());
+        } else {
+            v.setText("not good: " + ((session == null ) ? "null" : session.getState().toString()));
+        }     
+    }
+    
+    private class SessionStatusCallback implements Session.StatusCallback {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            updateView();
+        }
+    }
 }
